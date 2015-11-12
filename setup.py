ffd163cf1e9ef82fc4215f600794669c96add312
greenbender <byron.platt@gmail.com>
2015-11-12 12:09:27 +1100

    Fixed setup to add all packages

--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
 
 setup(
     name='django-gravy',
     version='0.1',
-    packages=['gravy'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD License',
     description='Reusable extras for Django (the gravy).',
     long_description=README,
     author='greenbender',
-    author_email='yourname@example.com',
+    zip_safe=False,
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
