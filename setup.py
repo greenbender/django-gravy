0d992acf02152946a76109b8d2eba56623221f0c
greenbender <byron.platt@gmail.com>
2015-11-16 14:16:35 +1100

    Version increment

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-gravy',
-    version='0.1.2',
+    version='0.1.3',
     packages=find_packages(),
 
     # only install the most common requirements
