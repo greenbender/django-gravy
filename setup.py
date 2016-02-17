2e197dbcb57cb75d8dd6e561b893bb08f626226b
greenbender <byron.platt@gmail.com>
2016-02-17 17:14:45 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-gravy',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
 
     # only install the most common requirements
