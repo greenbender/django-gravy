ac22cfa8b22d52edd9f0a14e61e2bfddc8e2e9c6
kp <dev22581@dev.tcd.lan>
2018-02-23 15:47:17 +1100

    updated registration to allow auto_user_add to automatically add user when a subclass of the class contained the user field is created

--- gravy/db/models/registration.py
+++ gravy/db/models/registration.py
@@ -1,12 +1,20 @@
+import logging
+
+log = logging.getLogger('gravy.db.models')
+
 class CurrentUserFieldRegistry(object):
     _registry = {}
-
+    
     def add_field(self, model, field):
         reg = self.__class__._registry.setdefault(model, [])
         reg.append(field)
 
     def get_fields(self, model):
-        return self.__class__._registry.get(model, [])
+        fields = []
+        for m, f in self.__class__._registry.items():
+            if issubclass(model, m):
+                fields.extend(f)
+        return fields
 
     def __contains__(self, model):
-        return model in self.__class__._registry
+        return issubclass(model, tuple(self.__class__._registry.keys()))
